30612a2974342c9441090d7c35850cd7670d9e3f
João Trevizoli Esteves <joao@lab804.com.br>
2017-05-25 22:55:55 -0300

    documentation

--- lib/WiFiManager/WiFiManager.hpp
+++ lib/WiFiManager/WiFiManager.hpp
@@ -1,3 +1,14 @@
+/*WiFiManager
+    __  _       _      ___   ___  _  _    __
+  / / | | __ _| |__  ( _ ) / _ \| || |   \ \
+/ /   | |/ _` | '_ \ / _ \| | | | || |_   \ \
+\ \   | | (_| | |_) | (_) | |_| |__   _|  / /
+ \_\  |_|\__,_|_.__/ \___/ \___/   |_|   /_/
+Interface of the WiFiManager object
+This object is responsable for facilitating the management
+of the the wifi connection.
+Created by: Joao Trevizoli Esteves
+*/
 
 #ifndef WIFICONN_HPP
 #define  WIFICONN_HPP
