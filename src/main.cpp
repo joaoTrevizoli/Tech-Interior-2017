50c8e19a29cba2701775073a7c025e3d48487dec
João Trevizoli Esteves <joao@lab804.com.br>
2017-05-25 23:42:57 -0300

    Soil moisture 01

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include "Arduino.h"
-#include "WiFiManager.hpp"
 
-WiFiManager wifi(WIFI_SSID, WIFI_PASSWORD);
+uint16_t rawHumidity(uint pin);
+float mapSoilHumidity(uint16_t humidity, uint8_t minReading, uint maxReading);
+
+uint analogPin = A0;
+uint16_t humidity;
 
 void setup()
 {
   Serial.begin(115200);
-  wifi.begin();
+  pinMode(analogPin, INPUT);
 }
 
 void loop()
 {
-  wifi.checkWifi();
+  humidity = rawHumidity(A0);
+  Serial.println("Raw humidity: ");
+  Serial.println(humidity);
+  Serial.print("Muisture in percentage: ");
+  Serial.println(mapSoilHumidity(humidity, 0, 100));
   delay(500);
+}
+
+uint16_t rawHumidity(uint pin)
+{
+  return (uint16_t)analogRead(pin);
+}
+
+float mapSoilHumidity(uint16_t humidity, uint8_t minReading, uint maxReading)
+{
+  uint16_t sensorValue = map(humidity, 1024, 0, minReading, maxReading*100);
+  float moisture = static_cast<float>(sensorValue)/100;
+  return moisture;
 }
