51a273538836dfb826905fb113f403064c3eeb45
João Trevizoli Esteves <joao@lab804.com.br>
2017-05-26 23:23:51 -0300

    Publishing

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,49 @@
 #include "Arduino.h"
 #include "SoilMoisture.hpp"
 #include "WiFiManager.hpp"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 
 WiFiManager wifi(WIFI_SSID, WIFI_PASSWORD);
 IPAddress localIp;
 
-SoilMoisture soilMoisture(A0);
+SoilMoisture soilMoistureSensor(A0);
+
+WiFiClientSecure wifiClient;
+Adafruit_MQTT_Client mqtt(&wifiClient, "mqtt.labmet.tech", 8883, \
+ MQTT_ID, MQTT_PASSWORD);
+
+ Adafruit_MQTT_Publish publisher(&mqtt, "/tech_interior/");
 
 void setup()
 {
   Serial.begin(115200);
   wifi.begin();
-
-  soilMoisture.begin();
+  mqtt.connect();
+  soilMoistureSensor.begin();
 }
 
 void loop()
 {
   wifi.checkWifi();
-  Serial.print("Humidity in percentage: ");
-  Serial.println(soilMoisture.soilHumidityPercent(0, 100));
+  uint16_t soilMoisture = soilMoistureSensor.soilHumidityPercent(0, 100);
+
+  if(mqtt.ping() != 0 | mqtt.connect())
+  {
+    Serial.println("We can publish!");
+    if(!publisher.publish(soilMoisture))
+    {
+      Serial.println("Not published...");
+    }
+    else
+    {
+      Serial.println("published");
+
+    }
+  }
+  else
+  {
+    Serial.println("We can't publish :( ");
+  }
   delay(500);
 }
